724394b3e1ccfe11ca85e0456dc8ab48ca4dbe1d
Peter Elger <elger.peter@gmail.com>
2015-05-08 07:21:17 +0100

    fish

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var app = require('express')();
 
 app.get('/ping', function(req, res){
-  res.send(new Date().toISOString());
+  res.send('fish fish fish fish: ' + new Date().toISOString());
 });
 
 app.listen(3000);
